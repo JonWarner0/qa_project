e64bf6eab6f6d95d2a72debd7f409fdca78425ea
Jon Warner <jonathanstaynerwarner@gmail.com>
2020-11-03 13:26:03 -0700

    + Sentence recognition, response.txt | Only outputs the same sentence.

--- qa.py
+++ qa.py
@@ -1,73 +1,87 @@
 import os
 import sys
-import nltk # write script to install externals in grading env
+import nltk 
 from nltk import word_tokenize
 from nltk.stem import PorterStemmer as ps
-from nltk import wordnet as wn
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+import nltk.data
 import re
 from nerd import ner
 
-#Wordnet quick references
-#https://www.nltk.org/howto/wordnet.html
+# nltk & Wordnet quick reference
+# https://www.nltk.org/howto/wordnet.html
 
+
+""" -------- Global Definitions and Setup -------- """
+sent_detector = None # Extract sentences
+try:
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+except Exception:
+    nltk.download('punkt')
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+
+""" -------- Class Definitions -------- """
 class qa:
     ID = ""
     question = ""
     ans = None
     dif = ""
     pos_tags = None 
-    root_entity = None 
+    root_entity = None # roots of each word
+
     def __init__(self, ID, q, ans=None, dif=None):
         self.ID = ID
         self.question = q
         self.ans = ans
         self.dif = dif
         self.pos_tags = nltk.pos_tag(word_tokenize(q))
-        #self.root_entity = [ps().stem(str(s)) for s in ner.name(q)]
-        self.root_entity = [wn.morph(s) for s in self.question]
+        self.root_entity = [ps().stem(str(s)) for s in ner.name(q)] # Not useful?
         
 
 class story:
     title = ""
     ID = ""
     date = ""
-    corpus = None
+    corpus = None #list of sentences
     pos_tags = None
-    root_entity = []
+    root_entity = [] # roots of each word
+
     def __init__(self, title, ID, date, corpus):
         self.title = title
         self.ID = ID
         self.date = date
-        self.corpus = corpus
+        self.corpus = sent_detector.tokenize(corpus) 
         self.pos_tags = [nltk.pos_tag(word_tokenize(t)) for t in self.corpus]
-        # currently paragraphs in corpus instead of single sentences.
         for p in self.corpus:
-            self.root_entity.append([wn.morph(s) for s in p])
-            #self.root_entity.append([ps().stem(s) for s in p])
-      
+            self.root_entity.append([ps().stem(s) for s in p.split()])
 
+
+""" -------- QA algorithm -------- """
 
 def extractStory(_path):
+    """ Extract story info and return a story object """
     input_data = []
     with open(_path, "r") as path:
         input_data = [line for line in path]
-    title = input_data[0].replace("HEADLINE: ","")
-    date = input_data[1].replace("DATA: ", "")
+    title = input_data[0].replace("HEADLINE: ","") # Extract labels
+    date = input_data[1].replace("DATE: ", "")
     ID = input_data[2].replace("STORYID: ", "")
-    index = input_data.index("TEXT:\n") + 2 # start of text
-    corpus = list(filter('\n'.__ne__,input_data[index:]))
-    corpus = [line.strip('\n') for line in corpus]
-    story_o = story(title, ID, date, corpus)
-    return story_o
+    index = input_data.index("TEXT:\n") + 2 # marker of text start
+    corpus = " ".join([line.strip('\n') for line in input_data[index:]])
+    story_ = story(title, ID, date, corpus)
+    return story_
 
 
 def extractQuestions(_path):
+    """ Create a list of qa objects to hold all question/answer data for output """ 
     questions = []
     with open(_path, "r") as path:
         q = []
-        for line in path:
+        for line in path: # Extract labels
             if line == '\n':
-                ID = q[0].replace("QuestionsID: ", "")
+                ID = q[0].replace("QuestionID: ", "").strip('\n')
                 question = q[1].replace("Question: ", "")
                 dif = q[2].replace("Difficulty: ", "")
                 questions.append(qa(ID, question, None, dif))
@@ -77,29 +91,40 @@ def extractQuestions(_path):
     return questions
 
 
-def questionMorph(question):
+def questionProcessing(question):
+    """ Rewrite with most likely sense and attempt to match corpus """
     return []
 
 
 def generateAnswer(story, question):
+    """ Computes overlap of root words. Includes stop words at this time """
     candidates = []
-    entities = set(question.root_entity)
+    q_entities = set(question.root_entity)
     for i in range(len(story.root_entity)): # root word overlap
-        candidates.append((i, set(story.root_entity[i]).union(entities)))
-    return sorted(candidates, key=lambda k: len(k[1]), reverse=True)
+        candidates.append((i, set(story.root_entity[i]).union(q_entities)))
+    best = sorted(candidates, key=lambda k: len(k[1]))[-1] #most overlap
+    return story.corpus[best[0]] # Sentence that most likely contains answer
+
+
+def outputResponseFile(path, answers):
+    """ Generates the Response.txt file with questions and answers """ 
+    with open(path+'/Response.txt', 'w') as file:
+        for q_a in answers:
+            file.write('QuestionID: {}\nAnswer: {}\n\n'.format(q_a.ID, q_a.ans))
 
 
 if __name__ == "__main__":
     directory_files = []
-    with open(sys.argv[1], "r") as input_file:
+    with open(sys.argv[1], "r") as input_file: # Extract all files
         for line in input_file:
             directory_files.append(line.strip('\n'))
     directory = directory_files[0]
     files = directory_files[1:]
-    for current_file in files:
+    all_questions_answers = []
+    for current_file in files: # Begin generating answers
         story = extractStory(directory+current_file+".story")
         questions = extractQuestions(directory+current_file+".questions")
         for q in questions:
-            answers = generateAnswer(story, q)
-            print(answers[0])
-        
+            q.ans = generateAnswer(story, q)
+            all_questions_answers.append(q)
+    outputResponseFile(os.getcwd(), all_questions_answers)
